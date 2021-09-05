f7c74365207f4e025f6e2249c7403932a8ed608c
Matt <58913953+ms5984@users.noreply.github.com>
2021-09-05 14:45:22 -0400

    Fix fatal javadoc + redesign build * AbstractColorUtil: since, not see * pom.xml: Moved source+javadoc processing into 'dev-build' active by default profile so it is easier to catch javadoc errors during development and we can still disable in a pinch. Can be combined with CI so we can be alerted if we don't check manually.

--- hermes-common/src/main/java/com/github/ms5984/hermes/util/AbstractColorUtil.java
+++ hermes-common/src/main/java/com/github/ms5984/hermes/util/AbstractColorUtil.java
@@ -39,7 +39,7 @@ import java.util.regex.Pattern;
 /**
  * Base class for utilities concerning color translation and null-sanitization.
  *
- * @see 1.0.0
+ * @since 1.0.0
  */
 public abstract class AbstractColorUtil {
     /**
