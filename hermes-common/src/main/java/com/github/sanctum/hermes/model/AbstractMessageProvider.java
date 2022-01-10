a8bdfd6b9b9dd13fa81c88de39109e449288db2c
Matt <58913953+ms5984@users.noreply.github.com>
2022-01-09 21:41:00 -0500

    Re-license * Main components solely as Apache 2.0 * hermes-standalone plugin module as LGPLv3.0 (swappable)

--- hermes-common/src/main/java/com/github/sanctum/hermes/model/AbstractMessageProvider.java
+++ hermes-common/src/main/java/com/github/sanctum/hermes/model/AbstractMessageProvider.java
@@ -1,26 +1,12 @@
 /*
- *  Copyright 2021 ms5984 (Matt) <https://github.com/ms5984>
- *
- *  This file is part of Hermes.
- *
- *  Hermes is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as
- *  published by the Free Software Foundation, either version 3 of the
- *  License, or (at your option) any later version.
- *
- *  Hermes is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  Copyright 2022 ms5984 (Matt) <https://github.com/ms5984>
+ *  Copyright 2022 Sanctum <https://github.com/the-h-team>
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may use this file in compliance with the License or aforementioned.
+ *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
